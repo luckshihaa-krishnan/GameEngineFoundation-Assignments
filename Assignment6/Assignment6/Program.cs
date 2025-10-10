b642d9d7adfc9c3538d47ce29b52056bb7155560
Luckshihaa Krishnan <luckshikrishnan@yahoo.ca>
2025-10-10 17:44:55 -0400

    commit to new branch

---
+++ Assignment6/Assignment6/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Name: Luckshihaa Krishnan 
+ * Student ID: 186418216
+ * Section: GAM 531 NSA 
+ */
+using Assignment6;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace WindowEngine
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(800, 600),
+                Title = "OpenTk Review - Camera",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
